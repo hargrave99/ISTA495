6a6eac4b4c9d67b0699f54ce87e60ea9c6ccc870
habahram <hamid.bahramian@gmail.com>
2020-11-19 12:04:24 -0500

    .

--- lectures/aggregate_example.py
+++ lectures/aggregate_example.py
@@ -1,7 +1,7 @@
 
 
 listRDD = sc.parallelize([1,2,3,4], 2)
-eqOp = (lambda local_result, list_element: (local_result[0] + list_element, local_result[1] + 1) )
+seqOp = (lambda local_result, list_element: (local_result[0] + list_element, local_result[1] + 1) )
 combOp = (lambda some_local_result, another_local_result: 
                (some_local_result[0] + another_local_result[0], some_local_result[1] + another_local_result[1]) )
 listRDD.aggregate( (0, 0), seqOp, combOp)
